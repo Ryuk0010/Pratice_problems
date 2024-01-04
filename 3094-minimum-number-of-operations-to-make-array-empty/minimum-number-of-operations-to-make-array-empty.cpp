1d7a82eeae92a5446a2d019d1312812a0710fd59
Ryuk0010 <142692479+Ryuk0010@users.noreply.github.com>
2024-01-04 14:49:24 +0530

    Time: 135 ms (26.15%) | Memory: 85.1 MB (63.17%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -8,8 +8,23 @@ public:
         int op = 0;
         for(auto it: mpp){
             int a = it.second;
+            int rem = 0;
             if(a == 1) return -1;
-            op += (a + 2)/3;
+            if(a % 3 == 0){
+                op += a/ 3;
+            }
+            else if(a % 3 == 1){
+                a -= 4;
+                op += a / 3;
+                op += 2;
+            }
+            else if(a % 3 == 2){
+                a -= 2;
+                op += a / 3;
+                op += 1;
+            }
+            
+            // op += (a + 2)/3;
         }
         return op;
     

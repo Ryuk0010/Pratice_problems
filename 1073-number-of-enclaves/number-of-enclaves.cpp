8ddfec7a7b1cb44092d939b5f29644b8893d9b40
Ryuk0010 <142692479+Ryuk0010@users.noreply.github.com>
2024-01-07 12:23:54 +0530

    Time: 57 ms (64.96%) | Memory: 33.2 MB (37.51%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -29,17 +29,17 @@ public:
         // }
         // for(int j = 1; j < m-1; j++){
         //   if(grid[j][0] == 1) dfs(j, 0, vis, grid, count);
-        //   if(grid[m-1][j] == 1) dfs(m-1, j, vis, grid, count);
+        //   if(grid[j][m-1] == 1) dfs(j, m-1, vis, grid, count);
         // }
-for( int i = 0; i<n; i++ ){
-            for( int j = 0 ; j<m; j++ ){
-                if( i == 0 || i == n - 1 || j == 0 || j == m - 1 ){
-                    if( grid[i][j] == 1 && vis[i][j] == -1){
-                        dfs(i,j,vis,grid,count);
+        for( int i = 0; i<n; i++ ){
+                    for( int j = 0 ; j<m; j++ ){
+                        if( i == 0 || i == n - 1 || j == 0 || j == m - 1 ){
+                            if( grid[i][j] == 1 && vis[i][j] == -1){
+                                dfs(i,j,vis,grid,count);
+                            }
+                        }
                     }
                 }
-            }
-        }
 
         int land = 0;
         for(int i = 0; i < n; i++){

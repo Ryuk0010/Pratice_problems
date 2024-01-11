eb3ba3767675b9ec0e5e5ca2a5d88e9b3837a1a2
Ryuk0010 <142692479+Ryuk0010@users.noreply.github.com>
2024-01-11 18:45:47 +0530

    Time: 15 ms (84.20%) | Memory: 14.2 MB (66.84%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,54 +1,40 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>> &prerequisites){
-        vector<int> adj[numCourses];
-
-        // Creating Adjacency List
-        for (auto it : prerequisites){
-            adj[it[0]].push_back(it[1]);
-        }
-
-        return using_kahnAlgo(numCourses, adj);
-    }
-
-    bool using_kahnAlgo(int V, vector<int> adj[]){
-
+      vector<int> adj[numCourses];
+      for (auto it : prerequisites){
+          adj[it[1]].push_back(it[0]);
+      }
+      int V = numCourses;
         vector<int> indegree(V, 0);
-
         for (int i = 0; i < V; i++){
-            for (int adjNode : adj[i]){
-                indegree[adjNode]++; // calculating indegree of every node
-            }
+          for (int adjNode : adj[i]){
+            indegree[adjNode]++; // calculating indegree of every node
+          }
         }
-
         queue<int> q;
         for (int i = 0; i < V; i++){
-            if (indegree[i] == 0){
-                q.push(i); // pushing starting sources
-            }
+          if (indegree[i] == 0){
+            q.push(i); // pushing starting sources
+          }
         }
 
         vector<int> topo;
         while (!q.empty()){
-            int node = q.front();
-            q.pop();
-
-            topo.push_back(node);
-
-            for (int adjNode : adj[node]){
-                indegree[adjNode]--;
-
-                // new start points
-                if (indegree[adjNode] == 0){
-                    q.push(adjNode);
-                }
+          int node = q.front();
+          q.pop();
+          topo.push_back(node);
+          for (int adjNode : adj[node]){
+            indegree[adjNode]--;
+            if (indegree[adjNode] == 0){
+              q.push(adjNode);
             }
+          }
         }
-
         if (topo.size() == V){
-            return true;
+          return true;
         }
 
-        return false;
+      return false;
     }
 };

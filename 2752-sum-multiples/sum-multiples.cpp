efda6e1b27200c3f835fc5df95f2aa066b12145a
Ryuk0010 <142692479+Ryuk0010@users.noreply.github.com>
2024-01-12 18:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (18.46%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+      int sum = 0;
+      while(n){
+        if(n%3 == 0) sum+= n;
+        else if(n%5 == 0) sum+= n;
+        else if(n%7 == 0)sum += n;
+        n--;
+      }
+      return sum;
+        
+    }
+};

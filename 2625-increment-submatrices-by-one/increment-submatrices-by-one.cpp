2671b7d740c3c7c33e6ab4ab9eba59d7ffee683f
Ryuk0010 <142692479+Ryuk0010@users.noreply.github.com>
2024-02-18 02:29:49 +0530

    Time: 169 ms (88.14%) | Memory: 85.4 MB (69.49%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -4,16 +4,20 @@ public:
         int sr, sc, er, ec;
         vector<vector<int>> mat(n, vector<int>(n, 0));
         for(int i = 0; i < queries.size(); i++){
-            sr = queries[i][0];
-            sc = queries[i][1];
-            er = queries[i][2];
-            ec = queries[i][3];
-            for(int j = sr; j <= er; j++){
-                for(int k = sc; k <= ec; k++){
-                    mat[j][k] = mat[j][k] + 1;
-                }
-            }
+          sr = queries[i][0];
+          sc = queries[i][1];
+          er = queries[i][2];
+          ec = queries[i][3];
+          for(int j = sr; j <= er; j++){
+            mat[j][sc] += 1;
+            if(ec < n-1) mat[j][ec+1] -= 1;
+          }
         }
-        return mat;
+        for(int i = 0; i < n; i++){
+          for(int j = 1; j < n; j++){
+            mat[i][j] += mat[i][j-1];
+          }
+        }
+      return mat;
     }
 };

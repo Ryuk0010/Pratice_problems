52096ed9a3bb51ba2021e84145ec83359ad79696
Ryuk0010 <142692479+Ryuk0010@users.noreply.github.com>
2024-02-21 21:16:06 +0530

    Time: 2 ms (54.18%) | Memory: 7.1 MB (29.81%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -5,9 +5,9 @@ public:
         int start = 1;
         int end = num;
         while(start <= end){
-            int mid = start + ((end - start) / 2);
-            if(num % mid == 0 && mid == num / mid) return true;
-            else if(mid > num / mid){
+            double mid = start + ((end - start) / 2);
+            if(mid*mid == num) return true;
+            else if(mid*mid > num){
                 end = mid - 1;
             }
             else{

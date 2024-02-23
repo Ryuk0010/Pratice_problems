89dd6e304c6cd4de79fa480f62544c9395393e9f
Ryuk0010 <142692479+Ryuk0010@users.noreply.github.com>
2024-02-24 01:29:25 +0530

    Time: 763 ms (25.00%) | Memory: 175.9 MB (11.46%) - LeetSync

---
+++ 1957-closest-room/closest-room.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> closestRoom(vector<vector<int>>& rooms, vector<vector<int>>& queries) {
+        for(int i = 0; i < queries.size(); i++) queries[i].push_back(i);
+
+        sort(queries.begin(), queries.end(), [](auto &a, auto &b) {
+            
+            return a[1] > b[1];
+        });
+        for(auto it : queries){
+            cout << it[0] << " " << it[1] << " " << it[2] << endl; 
+        }
+        cout << endl;
+        sort(rooms.begin(), rooms.end(), [](auto &a, auto &b){
+            return a[1] > b[1];
+        });
+            for(auto it : rooms){
+            cout << it[0] << " " << it[1] << endl; 
+        }
+        int i = 0;
+        set<int> st;
+        vector<int> ans(queries.size());
+        for(auto q : queries) {
+            int prefer = q[0], minSize = q[1], idx = q[2];
+            while(i < rooms.size() && rooms[i][1] >= minSize)
+                st.insert(rooms[i++][0]);
+            if(st.size()) {
+                auto it = st.upper_bound(prefer);
+                int res = it != st.end() ? abs(*it - prefer) : INT_MAX;
+                int resRoomId = it != st.end() ? *it : INT_MAX;
+                if(it != st.begin()) {
+                    --it;
+                    if(abs(*it - prefer) <= res)
+                        resRoomId = *it;
+                }
+                ans[idx] = resRoomId;
+            }else
+                ans[idx] = -1;
+        }
+        return ans;
+    }
+};

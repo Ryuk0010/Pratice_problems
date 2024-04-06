8c9490aad355815450965cdcabfe5db1fd2e673e
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-04-06 07:21:52 +0530

    Time: 9 ms (9.48%) | Memory: 16 MB (13.06%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void solve(int m, int n, string seq, vector<string>& ans){
+        if(m == 0 && n == 0){
+           ans.push_back(seq); 
+           return;
+        } 
+        if(m == n){
+            string seq1 = seq;
+            seq1.push_back('(');
+            solve(m-1, n, seq1, ans);
+        }
+        else if(m == 0){
+            string seq1 = seq;
+            seq1.push_back(')');
+            solve(m, n-1, seq1, ans);
+        }
+        else if(n == 0){
+            string seq1 = seq;
+            seq1.push_back('(');
+            solve(m-1, n, seq1, ans);
+        }
+        else{
+            string seq1 = seq;
+            string seq2 = seq;
+            seq1 += '(';
+            seq2.push_back(')');
+            solve(m-1, n, seq1, ans);
+            solve(m, n-1, seq2, ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string seq = "";
+        solve(n, n, seq, ans);
+        return ans;
+    }
+};

1ec113f602a75c5ccf7fbee2c0b889194069e64e
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-07-01 22:25:11 +0530

    Time: 3 ms (93.56%) | Memory: 17.7 MB (98.08%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.cpp
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -3,12 +3,12 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         int i = 0, j = 0;
         for(int it: pushed){
-            pushed[i++] = it;
+            pushed[i] = it;
+            i++;
             while(i > 0 && pushed[i-1] == popped[j]){
                 i--;
                 j++;
             }
-            
         }
         return i == 0;
     }

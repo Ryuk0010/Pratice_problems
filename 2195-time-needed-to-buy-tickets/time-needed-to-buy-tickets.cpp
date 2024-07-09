76dc663aa158abf1fc33327e7c550050c11debf5
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-07-09 22:41:40 +0530

    Time: 4 ms (41.63%) | Memory: 9.4 MB (60.64%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n = tickets.size();
+        int ele = tickets[k];
+        int time = ele * n;
+        for(int i = 0; i < n; i++){
+            int timeleft = ele - tickets[i];
+            // cout << timeleft << endl;
+            if(tickets[i] <= ele){
+                time -= timeleft;
+            }
+        }
+        int bigger = 0;
+        for(int i = k+1; i < n; i++){
+            if(tickets[i] >= tickets[k]) bigger++;
+        }
+        cout << time;
+        return time-bigger;
+    }
+};

30b1524b49c4a92238e03e8a515f9fa7e6fd26ef
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-07-23 02:33:59 +0530

    Time: 723 ms (28.36%) | Memory: 193.9 MB (20.18%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,36 +1,88 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<int> minDist(n, INT_MAX); // minimum distance to each point
-        vector<bool> inMST(n, false); // points already included in MST
-        minDist[0] = 0; // start from the first point
-        int sum = 0;
+    class DisjointSet{
+        public :
+        vector<int>parent ;
+        vector<int>size ; 
 
-        for (int i = 0; i < n; ++i) {
-            int u = -1;
+        DisjointSet(int n)
+        {
+            parent.resize(n+1,0) ;
+            size.resize(n+1,0) ;
 
-            // Find the point with the minimum distance to the MST
-            for (int j = 0; j < n; ++j) {
-                if (!inMST[j] && (u == -1 || minDist[j] < minDist[u])) {
-                    u = j;
-                }
-            }
-
-            sum += minDist[u];
-            inMST[u] = true;
-
-            // Update the minimum distances to the MST
-            for (int v = 0; v < n; ++v) {
-                if (!inMST[v]) {
-                    int weight = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
-                    if (weight < minDist[v]) {
-                        minDist[v] = weight;
-                    }
-                }
+            for(int i=0 ; i<=n ; i++)
+            {
+                parent[i]=i ;
+                size[i]=1 ;
             }
         }
 
-        return sum;
+        int findUPar(int u)
+        {
+            if(u==parent[u]) return u ;
+
+            return parent[u]=findUPar(parent[u]) ;
+        }
+
+        void UnionBySize(int u , int v)
+        {
+            int ulp_u = findUPar(u) ;
+            int ulp_v = findUPar(v) ;
+
+            if(ulp_u==ulp_v) return ;
+
+            if(size[ulp_u]<=size[ulp_v])
+            {
+                size[ulp_u]+=size[ulp_v] ;
+                parent[ulp_u]=ulp_v ;
+            }
+            else
+            {
+                size[ulp_v]+=size[ulp_u] ;
+                parent[ulp_v]=ulp_u ;
+            }
+        }
+    };
+    int Kruskal(vector<vector<int>>& edges)
+    {
+        int n = edges.size() ;
+        DisjointSet ds(n) ;
+
+        int sum = 0 ;
+
+        auto lambda = [&](vector<int>&vec1,vector<int>&vec2)
+        {
+            return vec1[2]<vec2[2] ;
+        };
+
+        sort(edges.begin(),edges.end(),lambda) ;
+
+        for(int i=0 ; i<n ; i++)
+        {
+            int u = edges[i][0] ;
+            int v = edges[i][1] ;
+            int wt = edges[i][2] ;
+
+            if(ds.findUPar(u)!=ds.findUPar(v))
+            {
+                ds.UnionBySize(u,v) ;
+                sum+=wt ;
+            }
+        }
+
+        return sum ;
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        vector<vector<int>>edges ;
+        for(int i=0 ; i<points.size() ; i++)
+        {
+            for(int j=i+1 ; j<points.size() ; j++)
+            {
+                int dist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                edges.push_back({i,j,dist}) ;
+            }
+        }
+
+        return Kruskal(edges) ;
     }
 };

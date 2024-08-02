fe1cbf5f419b17319acd0a0eb632ba181f43dc00
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-08-02 06:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.80%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -12,8 +12,19 @@ private:
     }
 public:
     int numDecodings(string s) {
+        if(s[0] == '0') return 0;
         int n = s.size();
-        vector<int> dp(n+1, -1);
-        return solve(s, n, 0, dp);
+        vector<int> dp(n+1, 0);
+        dp[0] = 1, dp[1] = 1;
+        // return solve(s, n, 0, dp);
+        for(int i = 2; i <= n; i++){
+            int one = s[i-1] - '0';
+            int two = stoi(s.substr(i-2, 2));
+
+            if(one != 0) dp[i] += dp[i-1];
+            if(10 <= two && two <= 26) dp[i] += dp[i-2];
+        }
+        return dp[n];
+        
     }
 };

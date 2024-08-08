9405a77da93e1d5d0a32f716a2964c4f47d1ec60
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-08-08 19:23:34 +0530

    Time: 64 ms (47.27%) | Memory: 24.7 MB (36.89%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -14,10 +14,33 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int ans = 0;
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        for(int i = n-1; i >= 0; i--){
-            for(int j = m-1; j >= 0; j--){
-                if(matrix[i][j]) ans = max (ans, solve(matrix, i, j, dp));
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        // for(int i = n-1; i >= 0; i--){
+        //     for(int j = m-1; j >= 0; j--){
+        //         if(matrix[i][j]) ans = max (ans, solve(matrix, i, j, dp));
+        //     }
+        // }
+        for(int j = 0; j < m; j++){
+            if(matrix[0][j] == '1') {
+                dp[0][j] = 1;
+                ans = 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                  dp[i][j]=matrix[i][j]-'0';
+                  if(dp[i][j]==1) ans=1;
+            }
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(dp[i][j] == 1) {
+                    int right = dp[i][j-1];
+                    int down = dp[i-1][j];
+                    int rw = dp[i-1][j-1];
+                    dp[i][j] = 1 + min({right, down, rw});
+                    ans = max(ans, dp[i][j]);
+                }
             }
         }
         return ans*ans;

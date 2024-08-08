4d33b01a9f87f6cb0a3dbddf682bb41c50e8084c
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-08-08 18:56:24 +0530

    Time: 77 ms (21.11%) | Memory: 25 MB (12.21%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int solve(vector<vector<char>>& matrix, int i, int j, vector<vector<int>> & dp){
-        if(i >= matrix.size() || j >= matrix[0].size() || matrix[i][j] == '0') return 0;
+        if(i < 0 || j < 0 || matrix[i][j] == '0') return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
-        int right = solve(matrix, i, j+1, dp);
-        int down = solve(matrix, i+1, j, dp);
-        int rw = solve(matrix, i+1, j+1, dp);
+        int right = solve(matrix, i, j-1, dp);
+        int down = solve(matrix, i-1, j, dp);
+        int rw = solve(matrix, i-1, j-1, dp);
 
         return dp[i][j] = 1 + min({right, down, rw});
     }
@@ -15,8 +15,8 @@ public:
         int m = matrix[0].size();
         int ans = 0;
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
                 if(matrix[i][j]) ans = max (ans, solve(matrix, i, j, dp));
             }
         }

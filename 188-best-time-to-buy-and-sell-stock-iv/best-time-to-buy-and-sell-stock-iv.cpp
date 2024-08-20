933105e43653a8961d4a0fc3c9e4f52ac39269f7
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-08-20 17:05:09 +0530

    Time: 11 ms (56.65%) | Memory: 16.8 MB (36.42%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -13,7 +13,18 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
         int buy = 1;
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k+1, vector<int>(2, -1)));
-        return prof(0, buy, k, prices, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        // return prof(0, buy, k, prices, dp);
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                for(int l=k;l>=1;l--){
+                    if(j == 0) 
+                        dp[i][j][l] = max(-prices[i]+dp[i+1][1][l],dp[i+1][0][l]);
+                    else
+                        dp[i][j][l] = max(prices[i]+dp[i+1][0][l-1],dp[i+1][1][l]);
+                }
+            }
+        }
+        return dp[0][0][k];
     }
 };

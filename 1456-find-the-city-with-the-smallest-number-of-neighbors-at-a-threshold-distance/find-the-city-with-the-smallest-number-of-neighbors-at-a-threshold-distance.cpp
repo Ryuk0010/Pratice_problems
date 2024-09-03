5d98fa17fc9dc23806e9034ba1d79da047ba91a5
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-09-04 01:57:24 +0530

    Time: 51 ms (19.74%) | Memory: 21.7 MB (14.45%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,52 +1,86 @@
 class Solution {
 public:
-    void floid(vector<vector<int>>& dist, int n){
-        for(int via = 0; via < n; via++){
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    
+    void floydWarshall(vector<vector<int>>& dist, int n) {
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j]);
-                    if(i == j) dist[i][j] = 0;
+                    if (i == j) dist[i][j] = 0;
                 }
             }
         }
     }
+
+    vector<int> dijkstra(int node, vector<vector<pair<int, int>>>& adj, int n) {
+        vector<int> dist(n, INT_MAX);
+        dist[node] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, node});
+
+        while (!pq.empty()) {
+            int u = pq.top().second;
+            pq.pop();
+
+            for (auto it : adj[u]) {
+                int v = it.first;
+                int wt = it.second;
+                if (dist[v] > dist[u] + wt) {
+                    dist[v] = dist[u] + wt;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+        return dist;
+    }
+
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<pair<int, int>>> adj(n);
+
+        for (auto it : edges) {
+            int src = it[0];
+            int dest = it[1];
+            int wt = it[2];
+            adj[src].push_back({dest, wt});
+            adj[dest].push_back({src, wt});
+        }
+
         vector<vector<int>> dist(n, vector<int>(n, 1e4 + 1));
-        
-        for(auto it: edges){
+
+        for (auto it : edges) {
             int src = it[0];
             int dest = it[1];
             int wt = it[2];
             dist[src][dest] = wt;
             dist[dest][src] = wt;
         }
-        floid(dist, n);
 
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                cout << dist[i][j] << " ";
-            }
-            cout << endl;
-        }
+        // floydWarshall(dist, n);
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         cout << dist[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
 
         int ans = -1;
         int miniCity = INT_MAX;
-        for(int i = 0; i < n; i++){
-            int cnt = 0;
-            for(int j = 0; j < n; j++){
-                if(i != j && dist[i][j] <= distanceThreshold){
-                   cnt++;
-                }
 
+        for (int i = 0; i < n; i++) {
+            vector<int> dist = dijkstra(i, adj, n);
+            int cnt = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (dist[j] <= distanceThreshold) {
+                    cnt++;
+                }
             }
-            if(cnt <= miniCity){
+
+            if (cnt <= miniCity) {
                 miniCity = cnt;
                 ans = i;
             }
         }
 
-    return ans;
+        return ans;
     }
 };

0b11fb36ec1dc07b0c04a8ab8eb028b6669c0f16
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-09-28 11:54:50 +0530

    Time: 471 ms (94.94%) | Memory: 288.7 MB (74.76%) - LeetSync

--- 3541-report-spam-message/report-spam-message.cpp
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
-        set<string> st(bannedWords.begin(), bannedWords.end());
+        unordered_set<string> st(bannedWords.begin(), bannedWords.end());
         int cnt = 0;
         for(auto it: message){
             if(st.count(it)) cnt++;

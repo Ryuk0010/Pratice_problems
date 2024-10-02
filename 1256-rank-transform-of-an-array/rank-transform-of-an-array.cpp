af057597e3e5cc497d14aa9afb5bce86828e12e6
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-10-02 10:08:06 +0530

    Time: 114 ms (14.38%) | Memory: 42.7 MB (74.45%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        map<int, int> mpp;
+        vector<int> copy(arr.begin(), arr.end());
+        sort(copy.begin(), copy.end());
+        int ind = 0;
+        for(int i = 0; i < copy.size(); i++){
+            if(!mpp.count(copy[i]))mpp[copy[i]] = ++ind;
+        }
+        for(int i = 0; i < arr.size(); i++){
+            if(mpp.count(arr[i])) copy[i] = mpp.find(arr[i])->second;
+        }
+        return copy;
+    }
+};

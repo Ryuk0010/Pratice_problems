d74de1baabeae67ab0145cf2152c5f0752bfa4a0
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-01-27 01:27:47 +0530

    Time: 21 ms (87.40%) | Memory: 95.2 MB (96.88%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,57 +1,25 @@
 class Solution {
 public:
-    void dijkstra(char &source, unordered_map<char, vector<pair<char, int>>> &adj, vector<vector<long long>> &costMatrix) {
-        priority_queue<pair<int, char>, vector<pair<int, char>>, greater<pair<int, char>>> pq;
-        pq.push({0, source});
-
-        while(!pq.empty()) {
-            int d = pq.top().first;
-            char adjNode = pq.top().second;
-            pq.pop();
-
-            for(auto &it : adj[adjNode]) {
-                char adjNode = it.first;
-                int cost = it.second;
-
-                if(costMatrix[source - 'a'][adjNode - 'a'] > d + cost) {
-                    costMatrix[source - 'a'][adjNode - 'a'] = d + cost;
-                    pq.push({d + cost, adjNode});
-                }
-            }
-
-        }
-        return;
+int D[26][26];
+void floid(vector<char>& original, vector<char>& changed, vector<int>& cost){
+    fill(&D[0][0], &D[0][0]+26*26, INT_MAX);
+    for(int i = 0; i < original.size(); i++){
+        D[original[i]-'a'][changed[i]-'a'] = min(D[original[i]-'a'][changed[i]-'a'], cost[i]);
     }
-
+    for(int i = 0 ; i < 26; i++) D[i][i] = 0;
+    for(int k = 0; k < 26; k++)
+        for(int i = 0; i < 26; i++)
+            for(int j = 0; j < 26; j++)
+                D[i][j] = min((long long)D[i][j], (long long)D[i][k]+D[k][j]);
+}
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        unordered_map<char, vector<pair<char, int>>> adj;
-
-        for(int i = 0; i < original.size(); i++) {
-            adj[original[i]].push_back({changed[i], cost[i]});
-        }
-
-        vector<vector<long long>> costMatrix(26, vector<long long>(26, INT_MAX));
-
-        for(int i = 0; i < source.length(); i++) {
-            char ch = source[i];
-            dijkstra(ch, adj, costMatrix);
-        }
-
+        floid(original, changed, cost);
         long long ans = 0;
-
-        for(int i = 0; i < source.length(); i++) {
-            if(source[i] == target[i]) {
-                continue;
-            }
-
-            if(costMatrix[source[i] - 'a'][target[i] - 'a'] == INT_MAX) {
-                return -1;
-            }
-
-            ans += costMatrix[source[i] - 'a'][target[i] - 'a'];
-
+        int n = source.size();
+        for(int i = 0; i < n; i++){
+            if(D[source[i]-'a'][target[i]-'a'] == INT_MAX) return -1;
+            ans+=D[source[i]-'a'][target[i]-'a'];
         }
-
         return ans;
     }
 };

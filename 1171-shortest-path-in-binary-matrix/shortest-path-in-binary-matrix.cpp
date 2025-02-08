4193dbd17c6d24c4ebc257eea6c69efcb45c810b
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-02-09 00:50:24 +0530

    Time: 14 ms (78.83%) | Memory: 26.2 MB (44.45%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -11,16 +11,17 @@ public:
         if(grid[0][0] == 1) return -1;
         q.push({1, {0, 0}});
         vis[0][0] = 1;
+        
         while(!q.empty()){
             int wt = q.front().first;;
             int xaxix = q.front().second.first;
             int yaxix = q.front().second.second;
             q.pop();
-            if(xaxix == grid.size() - 1 && yaxix == grid.size() - 1) return wt;
-            for(int i = 0; i < x.size(); i++){
+            if(xaxix == n - 1 && yaxix == n - 1) return wt;
+            for(int i = 0; i < 8; i++){
                 int tx = xaxix + x[i];
                 int ty = yaxix + y[i];
-                if(tx >= 0 && tx < grid.size() && ty >= 0 && ty < grid.size() && vis[tx][ty] == 0 && grid[tx][ty] == 0){
+                if(tx >= 0 && tx < n && ty >= 0 && ty < n && vis[tx][ty] == 0 && grid[tx][ty] == 0){
                     q.push({wt+1, {tx, ty}});
                     vis[tx][ty]=1;
                 }

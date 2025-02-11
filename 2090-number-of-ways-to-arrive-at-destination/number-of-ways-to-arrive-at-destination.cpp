7c5b493868efdca8de27ee11b3b02f8c45da18cc
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-02-11 14:30:56 +0530

    Time: 15 ms (36.40%) | Memory: 38.1 MB (41.66%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,40 +1,53 @@
 class Solution {
 public:
-    int countPaths(int n, vector<vector<int>>& roads) {
-       
-       vector<vector<pair<long long, long long>>> adj(n);
-        for(auto it:roads){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
-        }
-        priority_queue<pair<long long,long long>,
-        vector<pair<long long,long long> >,
-        greater<pair<long long,long long>>>pq;
+  
 
-        vector<long long>dist(n,1e12) , ways(n,0);
-        dist[0]=0;
-        ways[0]=1; long long mod= 1e9+7;
-        pq.push({0,0}) ;
-        while(!pq.empty()){
-            long long dis = pq.top().first;
-            long long node = pq.top().second;
-            pq.pop();
-            for(auto it:adj[node]){
-                long long adjNode=it.first;
-                long long edW=it.second;
-                
-                if(dis+edW<dist[adjNode]){
-                    dist[adjNode]=dis+edW;
-                    pq.push({dis+edW,adjNode});
-                    ways[adjNode]=ways[node]%mod;
-                }
-                else if(dis+edW==dist[adjNode]){
-                    ways[adjNode]=(ways[adjNode]+ways[node])%mod;
-                }
-            }
+int countPaths(int n, vector<vector<int>>& roads) {
+    int m = roads.size();
+    vector<vector<pair<long long, long long>>> adj(n);
 
-        }
-        return ways[n-1];
-        
+    for (int i = 0; i < m; i++) {
+        long long u = roads[i][0];
+        long long v = roads[i][1];
+        long long wt = roads[i][2];
+        adj[u].push_back({v, wt});
+        adj[v].push_back({u, wt});
     }
+
+    vector<long long> dist(n, LLONG_MAX);
+    vector<long long> ways(n, 0);
+    int mod = 1e9 + 7;
+
+    priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
+    
+   
+    dist[0] = 0;
+    ways[0] = 1;
+    pq.push({0, 0});  
+
+    while (!pq.empty()) {
+        long long cost = pq.top().first;
+        long long node = pq.top().second;
+        pq.pop();
+
+        // if (cost > dist[node]) continue;  
+
+        for (auto x : adj[node]) {
+            long long adjNode = x.first;
+            long long wt = x.second;
+
+            if (dist[adjNode] > cost + wt) {
+                dist[adjNode] = cost + wt;
+                ways[adjNode] = ways[node];  
+                pq.push({dist[adjNode], adjNode});
+            } 
+            else if (dist[adjNode] == cost + wt) {
+                ways[adjNode] = (ways[adjNode] + ways[node]) % mod; 
+            }
+        }
+    }
+
+    return ways[n - 1] % mod;
+}
+
 };

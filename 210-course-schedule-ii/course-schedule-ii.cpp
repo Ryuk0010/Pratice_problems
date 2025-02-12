cec4ff72d14aeca363e732b548fc3f2f8da7f77e
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-02-12 23:10:34 +0530

    Time: 11 ms (14.34%) | Memory: 19.4 MB (13.70%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
-      int n = pre.size();
-      vector<int> adj[numCourses];
-      vector<int> indegree(numCourses, 0);
-      queue<int> q;
-      for (auto it : pre){
-          adj[it[1]].push_back(it[0]);
-      }
-      for (int i = 0; i < numCourses; i++){
-          for (int adjNode : adj[i]){
-            indegree[adjNode]++;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        for(auto it: prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        for (auto adjNode : prerequisites){
+            indegree[adjNode[0]]++; 
+        }
+        
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++){
+          if (indegree[i] == 0){
+            q.push(i); 
           }
         }
-      for(int i = 0; i < numCourses; i++){
-        if(indegree[i] == 0)q.push(i);
-      }
-      vector<int> ans;
-      while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        ans.push_back(node);
-        for(auto it: adj[node]){
-          indegree[it]--;
-          if(indegree[it] == 0){
-            q.push(it);
+
+        vector<int> topo;
+        while (!q.empty()){
+          int node = q.front();
+          q.pop();
+          topo.push_back(node);
+          for (int adjNode : adj[node]){
+            indegree[adjNode]--;
+            if (indegree[adjNode] == 0){
+              q.push(adjNode);
+            }
           }
         }
-      }
-      if(ans.size() == numCourses) return ans;
-      else return {};
-      
+        return (topo.size() == numCourses) ? topo : vector<int>{};
     }
 };

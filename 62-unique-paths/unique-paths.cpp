abd10997c97631276ef03151b609a79fbbb46f82
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-09 01:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.48%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -10,11 +10,11 @@ public:
         return dp[m][n] = top + left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return uniquePathsHelper(m-1, n-1, dp);
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        // return uniquePathsHelper(m-1, n-1, dp);
         dp[0][0] = 1;
-        for(int i = 1; i < n; i++)dp[i][0] = 0;
-        for(int i = 1; i < m; i++)dp[0][i] = 0;
+        for(int i = 1; i < m; i++)dp[i][0] = 1;
+        for(int i = 1; i < n; i++)dp[0][i] = 1;
 
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){

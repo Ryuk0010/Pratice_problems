be9ab818743c3e3b2f191a75fc8c45edc091768c
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-09 00:51:29 +0530

    Time: 2 ms (18.38%) | Memory: 9.4 MB (8.94%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int ncr(int n, int r){
-        long long  ans = 1;
-        for(int i = 1; i <= r; i++){
-            ans = (ans*(n-i+1))/i; 
-        }
-        return (int)ans;
+    int uniquePathsHelper(int m, int n, vector<vector<int>>& dp){
+        if(m < 0 || n < 0) return 0;
+        if(m == 0 || n == 0) return 1;
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int top = uniquePathsHelper(m-1, n, dp);
+        int left = uniquePathsHelper(m, n-1, dp);
+        return dp[m][n] = top + left;
     }
     int uniquePaths(int m, int n) {
-        return ncr(m+n-2, min(m-1, n-1));
-    //     vector<vector<int>> dp(m,vector<int>(n,0));
-    //     if(m==1 && n==1) return 1;
-    //     for(int i=1;i<m;i++) dp[i][0]=1;
-    //     for(int i=1;i<n;i++)  dp[0][i]=1;
-
-    //     for(int i=1;i<m;i++){
-    //         for(int j=1;j<n;j++){
-    //             dp[i][j] = dp[i-1][j]+dp[i][j-1];           
-    //         }
-    //     }
-    //     return dp[m-1][n-1];
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return uniquePathsHelper(m-1, n-1, dp);
     }
 };

101c6b6c83b7723c2f34cab973a4c22020b6af6f
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-10 17:37:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (87.50%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
-        if(i >= nums.size()) return 0;
-        if(dp[i][j+1] != -1) return dp[i][j+1];
-        int ans1 = 0, ans2 = 0;
-        if(j == -1 || nums[i] > nums[j]) ans1 = 1 + solve(i+1, i, nums, dp);
-        ans2 = solve(i+1, j, nums, dp);
-        return dp[i][j+1] = max(ans1, ans2);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        vector<int> temp;
+        temp.push_back(nums[0]);
 
-        return solve(0, -1, nums, dp);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > temp.back()) temp.push_back(nums[i]);
+            else{
+                int idx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[idx] = nums[i];
+            }
+        }
+        return temp.size();
     }
 };

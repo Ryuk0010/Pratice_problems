76f40b906ef80e193f0316447d12e7a803ed1338
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-13 23:27:58 +0530

    Time: 55 ms (62.59%) | Memory: 26.3 MB (22.41%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+int dp1[2000][2000];
+    int dp2[2000];
+    bool isPal(string &s, int i, int j) {
+        if(i >= j)
+            return true;
+
+        if(dp1[i][j] != -1)
+            return dp1[i][j];
+        return dp1[i][j] = (s[i] == s[j]) && isPal(s, i+1, j-1);
+    }
+    int minCutHelp(string &s, int i) {
+        if(i == s.length() - 1) 
+            return 0;
+        if(dp2[i] != -1)
+            return dp2[i];
+        
+        dp2[i] = INT_MAX;
+
+        if(isPal(s, i, s.length() - 1))
+            return dp2[i] = 0;
+
+        for(int k = i; k < s.length(); ++k) {
+            if(!isPal(s, i, k))
+                continue;
+            dp2[i] = min(dp2[i], 1 + minCutHelp(s, k+1));
+        }
+        return dp2[i];
+    }
+public:
+    int minCut(string s) {
+        memset(dp1, -1, sizeof(dp1));
+        memset(dp2, -1, sizeof(dp2));
+        return minCutHelp(s, 0);
+    }
+};

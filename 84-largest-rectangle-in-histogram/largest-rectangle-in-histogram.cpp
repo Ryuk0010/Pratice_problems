c68a08c8b1a9736755f58f44f041bf708b49fd70
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-14 22:01:10 +0530

    Time: 27 ms (46.19%) | Memory: 81.2 MB (78.56%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    
-    vector<int> nextNse(vector<int>& heights){
-        stack<int> st;
-        int n = heights.size();
-        vector<int> ans(n, n);
-        
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && heights[st.top()] > heights[i]){
-                ans[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    
-
-    vector<int> prevPse(vector<int>& heights){
-        stack<int> st;
-        int n = heights.size();
-        vector<int> ans(n, -1);
-        
-        for(int i = n-1; i >= 0; i--){
-            while(!st.empty() && heights[st.top()] > heights[i]){
-                ans[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> nse = nextNse(heights);
-        vector<int> pse = prevPse(heights);
-
+        int n = heights.size();
+        stack<int> st;
         int ans = INT_MIN;
-        for(int i = 0; i < heights.size(); i++){
-            int w = nse[i] - pse[i] - 1;
-            ans = max(ans, w*heights[i]);
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                int w;
+                int hInd = st.top();
+                st.pop();
+                if(st.empty()) w = i;
+                else w = i - st.top() - 1;
+                ans = max(ans, heights[hInd] * w);
+                
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int w;
+            int hInd = st.top();
+            st.pop();
+            if(st.empty()) w = n;
+            else w = n - st.top() - 1;
+            ans = max(ans, heights[hInd] * w);
         }
         return ans;
     }

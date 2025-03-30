6756a3aa194907a1f368d2731e4ac849cbab1025
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-31 03:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 60.8 MB (82.68%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,26 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    
-    void markTree(TreeNode* root, TreeNode*& prev, TreeNode*& first, TreeNode*& end) {
-        if (!root) return;
-        markTree(root->left, prev, first, end);
-        if (prev) {
-            if (root->val < prev->val) {
-                if (!first) {
-                    first = prev;
-                }
+public:
+    void solve(TreeNode* root, TreeNode* &first, TreeNode* &end, TreeNode* &prev){
+        if(root == nullptr) return;
+        solve(root->left, first, end, prev);
+        if(prev){
+            if(root->val < prev->val){
+                if(!first) first = prev;
                 end = root;
             }
         }
         prev = root;
-        markTree(root->right, prev, first, end);
+        solve(root->right, first, end, prev);
     }
-    
-    
-public:
     void recoverTree(TreeNode* root) {
-        TreeNode *prev = nullptr, *first = nullptr, *end = nullptr;
-        markTree(root, prev, first, end);
+        TreeNode *first = nullptr;
+        TreeNode *end = nullptr;
+        TreeNode *prev = nullptr;
+        solve(root, first, end, prev);
         swap(first->val, end->val);
         return;
     }

ca8bb9db6e7b1dde13b26df64a5790612cb39d3e
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-30 18:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (87.37%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -4,11 +4,15 @@ public:
         string str = to_string(n);
         int i = str.length()-1;
         while(i > 0 && str[i] <= str[i-1]) i--;
+
         if(i == 0) return -1;
+        
         int j = str.length() - 1;
         while(i <= j && str[i-1] >= str[j]) j--;
+
         swap(str[i-1], str[j]);
         reverse(str.begin()+i, str.end());
+        
         long long result = stoll(str);
         if(result > INT_MAX){
             return -1;

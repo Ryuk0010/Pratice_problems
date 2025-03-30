e2643d52111128ff36d8e1545fe59408ffa8b30f
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-03-30 18:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (62.39%) - LeetSync

---
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string str = to_string(n);
+        int i = str.length()-1;
+        while(i > 0 && str[i] <= str[i-1]) i--;
+        if(i == 0) return -1;
+        int j = str.length() - 1;
+        while(i <= j && str[i-1] >= str[j]) j--;
+        swap(str[i-1], str[j]);
+        reverse(str.begin()+i, str.end());
+        long long result = stoll(str);
+        if(result > INT_MAX){
+            return -1;
+        }
+        return (int)result;
+    }
+};

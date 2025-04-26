d8fb4f707e2cf730e296c29502f857643649f6d8
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-04-27 03:46:39 +0530

    Time: 3 ms (18.25%) | Memory: 8.9 MB (50.80%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,30 +1,28 @@
 class Solution {
-private:
-    int solve(string s, int n, int ind, vector<int> &dp){
+public:
+    int solve(string s, int n, int ind, vector<int> dp){
         if(ind == n) return 1;
         if(dp[ind] != -1) return dp[ind];
         if(s[ind] == '0') return 0;
         int ans = solve(s, n, ind+1, dp);
-        if(ind+1 < n && (s[ind] == '1' || (s[ind] == '2' && s[ind+1] <= '6')))
-        ans += solve(s, n, ind+2, dp);
-
+        if(ind+1 < n && (s[ind] == '1' ||(s[ind] == '2' && s[ind+1] <= '6'))){
+            ans += solve(s, n, ind+2, dp);
+        }
         return dp[ind] = ans;
     }
-public:
     int numDecodings(string s) {
         if(s[0] == '0') return 0;
-        int n = s.size();
+        int n = s.length();
         vector<int> dp(n+1, 0);
-        dp[0] = 1, dp[1] = 1;
         // return solve(s, n, 0, dp);
+        dp[0]=1, dp[1] = 1;
         for(int i = 2; i <= n; i++){
-            int one = s[i-1] - '0';
-            int two = stoi(s.substr(i-2, 2));
+            int a = s[i-1] - '0';
+            int b = stoi(s.substr(i-2, 2));
 
-            if(one != 0) dp[i] += dp[i-1];
-            if(10 <= two && two <= 26) dp[i] += dp[i-2];
+            if(a != 0) dp[i] += dp[i-1];
+            if(b >= 10 && b <= 26) dp[i] += dp[i-2];
         }
         return dp[n];
-        
     }
 };

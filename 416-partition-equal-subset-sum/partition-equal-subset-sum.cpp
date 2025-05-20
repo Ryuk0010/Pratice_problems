593e17ace1405c8e3ecb2bc89d742345fc4d0b25
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-05-20 22:04:28 +0530

    Time: 187 ms (14.99%) | Memory: 110.5 MB (16.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool solve(vector<int>& nums, int i, int sum, int s2, vector<vector<int>> &dp){
-        if(i == nums.size()) return false;
+        if(i == nums.size() || s2 > sum) return false;
         if(sum  == s2) return true;
         if(dp[i][s2] != -1) return dp[i][s2];
 
@@ -13,14 +13,10 @@ public:
         int sum = 0;
         int n = nums.size();
         for(int i = 0; i < nums.size(); i++) sum += nums[i];
-        if(sum%2) return false;
-         vector<vector<int>> dp(n, vector<int>(sum, -1));
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < sum; j++){
+        if(sum % 2) return false;
+        sum = sum/2;
+        vector<vector<int>> dp(n, vector<int>(sum, -1));
 
-        //     }
-        // }
-
-        return solve(nums, 0, sum/2, 0, dp);
+        return solve(nums, 0, sum, 0, dp);
     }
 };

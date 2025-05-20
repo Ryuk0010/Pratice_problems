7b030fd06fd13a46e82136b160d29be7e601b9bd
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-05-20 21:48:28 +0530

    Time: 410 ms (5.01%) | Memory: 262.4 MB (5.17%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool solve(vector<int>& nums, int i, int sum, int s2, vector<vector<int>> &dp){
+        // cout << i <<" " << s2 << endl;
+        if(i == nums.size()) return false;
+        if(sum - s2 == s2) return true;
+        if(dp[i][s2] != -1) return dp[i][s2];
+        bool b = solve(nums, i+1, sum, s2, dp);
+
+        bool a = solve(nums, i+1, sum, s2 + nums[i], dp);
+        
+        return dp[i][s2] = a | b;
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        for(int i = 0; i < nums.size(); i++) sum += nums[i];
+        vector<vector<int>> dp(n, vector<int>(sum, -1));
+
+        return solve(nums, 0, sum, 0, dp);
+    }
+};

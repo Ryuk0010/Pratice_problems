bd204d85ddf856799122b0c76c45823f8707972a
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-05-26 18:05:52 +0530

    Time: 24 ms (79.21%) | Memory: 16.3 MB (76.65%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (grid[row][col] != '1') return;
+        grid[row][col] = '0';
+
+        if (row > 0 && grid[row - 1][col] == '1') dfs(grid, row - 1, col);
+        if (row < grid.size() - 1 && grid[row + 1][col] == '1') dfs(grid, row + 1, col);
+        if (col > 0 && grid[row][col - 1] == '1') dfs(grid, row, col - 1);
+        if (col < grid[0].size() - 1 && grid[row][col + 1] == '1') dfs(grid, row, col + 1);
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int res = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+        int cnt = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '1') {
-                    markIsland(grid, i, j);
-                    res++;
+                    cnt++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return res;
-    }
-    void markIsland(vector<vector<char>>& grid, int r, int c) {
-        if (grid[r][c] != '1') return;
-        grid[r][c] = '0';
-        if (r > 0 && grid[r - 1][c] == '1') markIsland(grid, r - 1, c);
-        if (r < grid.size() - 1 && grid[r + 1][c] == '1') markIsland(grid, r + 1, c);
-        if (c > 0 && grid[r][c - 1] == '1') markIsland(grid, r, c - 1);
-        if (c < grid[0].size() - 1 && grid[r][c + 1] == '1') markIsland(grid, r, c + 1);
+
+        return cnt;
     }
 };

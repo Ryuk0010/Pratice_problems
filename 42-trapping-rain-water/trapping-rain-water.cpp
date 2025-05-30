4d60b04236498c0c032ff42195ff97c972115696
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-05-30 18:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (30.84%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
         stack<int> st;
+        int n = height.size();
         int water = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && height[i] > height[st.top()]) {
+        for(int i = 0; i < n; i++){
+            while (!st.empty() && height[i] > height[st.top()]){
                 int top = st.top();
                 st.pop();
                 if (st.empty()) break;
 
                 int dist = i - st.top() - 1;
                 int h = min(height[i], height[st.top()]) - height[top];
-                water += dist * h;
+                water += dist*h;
             }
             st.push(i);
         }
-        
         return water;
     }
 };

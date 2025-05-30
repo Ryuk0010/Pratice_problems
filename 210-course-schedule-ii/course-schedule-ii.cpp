67dd33e0815c491285152b4ffd6c9718ca2f8222
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-05-30 21:42:29 +0530

    Time: 3 ms (74.57%) | Memory: 18.1 MB (66.85%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,32 +2,29 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-        for(auto it: prerequisites){
-            adj[it[1]].push_back(it[0]);
-            indegree[it[0]]++; 
+        vector<int> indeg(numCourses, 0);
+        for(int i = 0; i < prerequisites.size(); i++){
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            adj[b].push_back(a);
+            indeg[a]++;
         }
-
-        
         queue<int> q;
-        for (int i = 0; i < numCourses; i++){
-          if (indegree[i] == 0){
-            q.push(i); 
-          }
+        for(int i = 0; i < numCourses; i++){
+            if(indeg[i] == 0)q.push(i);
         }
-
-        vector<int> topo;
-        while (!q.empty()){
-          int node = q.front();
-          q.pop();
-          topo.push_back(node);
-          for (int adjNode : adj[node]){
-            indegree[adjNode]--;
-            if (indegree[adjNode] == 0){
-              q.push(adjNode);
+        vector<int> ans;
+        while(!q.empty()){
+            int ele = q.front();
+            ans.push_back(ele);
+            q.pop();
+            for(auto it: adj[ele]){
+                indeg[it]--;
+                if(indeg[it] == 0){
+                    q.push(it);
+                }
             }
-          }
         }
-        return (topo.size() == numCourses) ? topo : vector<int>{};
+        return (ans.size() == numCourses)? ans : vector<int> {};
     }
 };

fd980a63b8796b5401608aa56c97bed6e13af793
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-05-31 18:25:31 +0530

    Time: 343 ms (29.29%) | Memory: 21.2 MB (64.04%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        set<string> st;
+        int n = wordList.size();
+        for(int i = 0; i < n; i++) st.insert(wordList[i]);
+
         queue<pair<string, int>> q;
-        unordered_set<string> st;
-        st.insert(wordList.begin(), wordList.end());
         q.push({beginWord, 1});
         while(!q.empty()){
-          string word = q.front().first;
-          int step = q.front().second;
-          q.pop();
-          if(word == endWord) return step;
-          for(int i = 0; i < word.size(); i++){
-            char orig = word[i];
-            for(char ch = 'a'; ch <= 'z'; ch++){
-              word[i] = ch;
-              if(st.find(word) != st.end()){
-                q.push({word, step+1});
-                st.erase(word);
-              }
-
+            auto top = q.front();
+            q.pop();
+            string word = top.first;
+            int step = top.second;
+            if(word == endWord) return step;
+            
+            for(int j = 0; j < word.size(); j++){
+                char org = word[j];
+                for(int i = 'a'; i <= 'z'; i++){
+                    word[j] = i;
+                    if(st.find(word) != st.end()){
+                        q.push({word, step+1});
+                        st.erase(word);
+                    }
+                }
+                word[j] = org;
             }
-            word[i] = orig;
-          }
+            
         }
         return 0;
     }

d9ef03099910dfc26aac5bbb3de2899a82324265
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-06-16 19:31:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (74.17%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if (digits.empty()) return ans;
-
-        unordered_map<char, string> mp = {
-            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
-            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
-        };
-
-        dfs(digits, 0, "", ans, mp);
-        return ans;
-    }
-
-    void dfs(string digits, int i, string cur, vector<string>& ans, const unordered_map<char, string>& mp) {
+    void dfs(string& digits, int i, vector<string>& m, vector<string>& res, string& cur) {
         if (i == digits.size()) {
-            ans.push_back(cur);
+            res.push_back(cur);
             return;
         }
-
-        for (char ch : mp.at(digits[i])) {
-            dfs(digits, i + 1, cur + ch, ans, mp);
+        for (char c : m[digits[i] - '0']) {
+            cur.push_back(c);
+            dfs(digits, i + 1, m, res, cur);
+            cur.pop_back();
         }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        vector<string> res;
+        vector<string> m = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        string cur;
+        dfs(digits, 0, m, res, cur);
+        return res;
     }
 };

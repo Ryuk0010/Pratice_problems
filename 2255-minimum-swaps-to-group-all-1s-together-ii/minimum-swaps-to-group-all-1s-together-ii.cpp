02006319ae620237178375287da5409d856f9a72
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-06-20 20:46:46 +0530

    Time: 3 ms (85.86%) | Memory: 84.1 MB (94.01%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int k = accumulate(nums.begin(), nums.end(), 0);
+        int cnt = accumulate(nums.begin(), nums.begin() + k, 0);
+        int n = nums.size();
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            total = max(total, cnt);
+            cnt -= nums[i];
+            cnt += nums[(i+k) % n];
+        }
+        return k - total;
+    }
+};

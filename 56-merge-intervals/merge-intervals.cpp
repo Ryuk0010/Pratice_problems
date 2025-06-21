991bfbe14b788a322bf3a696e1fe3632c176c22d
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-06-21 16:23:49 +0530

    Time: 3 ms (89.60%) | Memory: 24 MB (33.18%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;
-        vector<pair<int, int>> temp;
+        if(intervals.size() <= 1) return intervals;
         sort(intervals.begin(), intervals.end());
-        int n = intervals.size();
-        int pFirstEle = intervals[0][0];
-        int pSecondEle = intervals[0][1];
-        for(int i = 1; i < n; i++){
-            int cFirstEle = intervals[i][0];
-            int cSecondEle = intervals[i][1];
-            if(cFirstEle <= pSecondEle){ 
-                pSecondEle = max(pSecondEle, cSecondEle);
+        vector<vector<int>> ans;
+        int prev1 = intervals[0][0];
+        int prev2 = intervals[0][1];
+        for(int i = 1; i < intervals.size(); i++){
+            int cur1 = intervals[i][0];
+            int cur2 = intervals[i][1];
+
+            if(cur1 <= prev2){
+                prev2 = max(prev2, cur2);
             }
             else{
-                ans.push_back({pFirstEle, pSecondEle});
-                pFirstEle = cFirstEle;
-                pSecondEle = cSecondEle;
+                ans.push_back({prev1, prev2});
+                prev1 = cur1;
+                prev2 = cur2;
             }
-
         }
-        ans.push_back({pFirstEle, pSecondEle});
+        ans.push_back({prev1, prev2});
         return ans;
     }
 };

3a8d363549452cda3c14dc7274700b47158bc72e
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-06-25 00:25:21 +0530

    Time: 15 ms (92.33%) | Memory: 17.3 MB (84.96%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
+    vector<vector<int>> fourSum(vector<int>& nums, int t) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-
         int n = nums.size();
-        for(int i = 0; i < n; i++)
-        {
-            if(i > 0 and nums[i] == nums[i - 1]) continue;
-            for(int j = i + 1; j < n; j++)
-            {
-                if(j > (i + 1) and nums[j] == nums[j - 1]) continue;
-                
-                int start = j + 1;
-                int end = n - 1;
 
-                while(start < end)
-                {
-                    long long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[start];
-                    sum += nums[end];
+        for(int i = 0; i <= n-4; i++){   // i == 0
+            if(i > 0  && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j <= n-3; j++){  // j=1
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                long long target = (long long)t - nums[i] - nums[j]; //tar = 0
 
-                    if(sum == target)
-                    {
-                        ans.push_back({nums[i] , nums[j] , nums[start] , nums[end]});
-                        int low = nums[start];
-                        int high = nums[end];
-                        start++,end--;
-                        while(start < end and nums[start] == low) start++;
-                        while(start < end and nums[end] == high) end--;
+            int k = j+1, l = n-1;// k=2, l=4
+                while(k < l){
+                    long long sum = nums[k] + nums[l];
+                    if(sum == target){
+
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(l > k && nums[l] == nums[l+1]) l--;
                     }
-                    else if(sum > target) end--;
-                    else start++;
+                    else if(sum > target){
+                        l--;
+                    }
+                    else k++;
                 }
             }
+            
         }
         return ans;
     }

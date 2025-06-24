cd26ac8e7fa5447268537591d91c7c9e20aefc3a
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-06-24 23:10:31 +0530

    Time: 232 ms (47.83%) | Memory: 196.8 MB (40.41%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -3,30 +3,30 @@ public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         vector<pair<int, int>> ele;
         int n = nums.size();
-        for(int i = 0; i < n; i++)ele.push_back({nums[i], i});
+        for(int i = 0; i < n; i++) ele.push_back({nums[i], i});
         sort(ele.begin(), ele.end());
 
-        vector<vector<pair<int, int>>> indPair;
-        indPair.push_back({ele[0]});
+        vector<vector<pair<int, int>>> indexPair;
+        indexPair.push_back({ele[0]});
         for(int i = 1; i < n; i++){
-            if(ele[i].first - ele[i-1].first <= limit) indPair.back().push_back({ele[i]});
-            else indPair.push_back({ele[i]});
+            if(ele[i].first - ele[i-1].first <= limit) indexPair.back().push_back({ele[i]});
+            else indexPair.push_back({ele[i]});
         }
 
-
         vector<int> ans(n, 0);
-        for(auto& it: indPair){
+        for(auto it: indexPair){
             vector<int> idx;
             for(auto [value, index]: it){
                 idx.push_back(index);
             }
+
             sort(idx.begin(), idx.end());
 
             for(int i = 0; i < idx.size(); i++){
                 ans[idx[i]] = it[i].first;
             }
-
         }
         return ans;
     }
 };
+

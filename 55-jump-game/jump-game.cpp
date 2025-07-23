5c40cf48e65aab3949c4d5a1d2d975fd2e104931
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-23 17:50:22 +0530

    Time: 556 ms (10.13%) | Memory: 55.8 MB (6.82%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,9 +4,9 @@ public:
         if(idx >= n-1) return true;
         if(dp[idx] != -1) return dp[idx];
         for(int i = 1; i <= nums[idx]; i++){
-            if(solve(nums, idx+i, n, dp)) return dp[idx] = true;
+            if(solve(nums, idx+i, n, dp)) return dp[idx] = 1;
         }
-        return dp[idx] = false;
+        return dp[idx] = 0;
     }
     bool canJump(vector<int>& nums) {
         vector<int> dp(nums.size(), -1);

996cb06f9fc228b2bba4f58116a68c4311babe68
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-24 18:03:14 +0530

    Time: 12 ms (84.17%) | Memory: 47.1 MB (62.57%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
+const int MOD = 1e9 + 7;
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-        vector<int> left(n, 0), right(n, 0);
-        stack<pair<int, int>> st;
+        vector<int> l(n), r(n);
+        stack<int> st;
         for(int i = 0; i < n; i++){
-            int cnt = 1;
-            while(!st.empty() && st.top().first > arr[i]){
-                cnt += st.top().second;
-                st.pop();
-            }
-            st.push({arr[i], cnt});
-            left[i] = cnt;
+            while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
+            l[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
-        st = stack<pair<int, int>>();
+        st = stack<int>();
         for(int i = n-1; i >= 0; i--){
-            int cnt = 1;
-            while(!st.empty() && st.top().first >= arr[i]){
-                cnt += st.top().second;
-                st.pop();
-            }
-            st.push({arr[i], cnt});
-            right[i] = cnt;
-        }
+            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+            r[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }   
         long long ans = 0;
         for(int i = 0; i < n; i++){
-            long long l = left[i] % MOD;
-            long long r = right[i] % MOD;
-            long long prod = (l * r) % MOD;
-            prod = (prod * arr[i]) % MOD;
-            ans = (ans + prod) % MOD;
-
-        }
-        return (int)ans;
+            long long d = ((r[i] - i) * (i - l[i])) % MOD;
+            long long s = (d * arr[i]) % MOD;
+            ans = (ans + s) % MOD;
+        }  
+        return (int)ans;   
     }
 };

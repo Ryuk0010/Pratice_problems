a00daa36a886acbaafb65657484b925bdb470e45
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-25 21:51:52 +0530

    Time: 7 ms (96.87%) | Memory: 59.2 MB (92.73%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        map<int, int, greater<int>> mpp;
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        int freq = 0;
-        for(auto it: mpp){
-            freq = it.second;
-            while(freq){
-                freq--;
-                k--;
-                if(k == 0) return it.first;
+    int solve(vector<int>& nums, int left, int right, int k){
+        if(left == right) return nums[left];
+
+        int s = left;
+        int e = right;
+        int p = nums[left];
+
+        while(s <= e){
+            while(s <= e && nums[s] < p) s++;
+            while(s <= e && nums[e] > p) e--;
+            if(s <= e){
+                swap(nums[s], nums[e]);
+                s++;
+                e--;
             }
-            
         }
-        return 0;
+        if(e >= k) return solve(nums, left, e, k);
+        else if(s <= k) return solve(nums, s, right, k);
+        else return nums[k];
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        k = nums.size() - k;
+        return solve(nums, 0, nums.size()-1, k);
     }
 };

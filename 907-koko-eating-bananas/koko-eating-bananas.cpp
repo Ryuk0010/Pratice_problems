ac48e3045b2ac1df8048eceb536875ba798a44da
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-26 15:26:39 +0530

    Time: 7 ms (73.15%) | Memory: 22.9 MB (73.82%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    long long timetaken(vector<int>& piles, int k)
-    {
-        long long ans=0;
-        for (int i=0; i<piles.size(); i++){
-            ans+= (piles[i]/k)+(piles[i]%k!=0);
+    long long solve(vector<int>& piles, int m){
+        long long ans = 0;
+        for(int i = 0; i < piles.size(); i++){
+            ans += (piles[i]/m) + (int)(piles[i]%m != 0);
         }
         return ans;
     }
-    int minEatingSpeed(vector<int>& piles, int h)
-     {
-        int low = 1; int high = *max_element(piles.begin(),piles.end());
-        while (low <= high)
-        {
-            int mid= low+ (high-low)/2;
-            if (timetaken(piles,mid)<=h)
-            {
-                high= mid-1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+
+        while(l <= r){
+            int m =  l + (r - l)/2;
+            if(solve(piles, m) <= h){
+                r = m - 1;
             }
-            else low= mid+1;
+            else l = m + 1;
         }
-        return low;
+        return l;
     }
 };
+// 3,6,7,11
+// l= 4
+// r= 3
+// m = 6, 3, 4, 
+// 1, 1, 1
+// h = 5
+// l = 1
+// h = 1
+// m = 

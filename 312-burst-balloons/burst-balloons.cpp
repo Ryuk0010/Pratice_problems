a65beb90a92945a909984b80364a7d6592508aa0
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-27 21:03:15 +0530

    Time: 203 ms (34.34%) | Memory: 13.9 MB (27.57%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -3,18 +3,25 @@ public:
     int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
         if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int ans = INT_MIN;
+        int ans = 0;
         for(int k = i; k <= j; k++){
-            int cost = nums[i-1] * nums[k] * nums[j+1] + solve(i, k-1, nums, dp) + solve(k+1, j, nums, dp);
-            ans = max(ans, cost);
-
+            int sum = nums[i-1]*nums[k]*nums[j+1] + solve(i, k-1, nums, dp) + solve(k+1, j, nums, dp);
+            ans = max(sum, ans); 
         }
         return dp[i][j] = ans;
     }
     int maxCoins(vector<int>& nums) {
-        nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1));
-        return solve(1, nums.size()-2, nums, dp);
+        nums.push_back(1);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int ans = solve(1, nums.size()-2, nums, dp);
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n; j++){
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return ans;
     }
 };

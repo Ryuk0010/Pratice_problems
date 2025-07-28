1c27bd223249b61b4141cd5cc19244514e8b3e22
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-28 20:03:24 +0530

    Time: 1624 ms (29.75%) | Memory: 9.4 MB (73.46%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,25 +1,21 @@
 class Solution {
-int dp1[2000][2000];
+// int dp1[2000][2000];
     int dp2[2000];
     bool isPal(string &s, int i, int j) {
         if(i >= j)
             return true;
-
-        if(dp1[i][j] != -1)
-            return dp1[i][j];
-        if(s[i] != s[j]) dp1[i][j] = 0;
-        if(s[i] == s[j]) dp1[i][j] = isPal(s, i+1, j-1);
-        return dp1[i][j];
-
+        bool ck;
+        // if(dp1[i][j] != -1)
+        //     return dp1[i][j];
+        if(s[i] != s[j]) ck = 0;
+        if(s[i] == s[j]) ck = isPal(s, i+1, j-1);
+        // return dp1[i][j];
+        return ck;
     }
     int minCutHelp(string &s, int i) {
-        if(i == s.length() - 1) 
-            return 0;
-        if(dp2[i] != -1)
-            return dp2[i];
+        if(i == s.length() - 1) return 0;
+        if(dp2[i] != -1) return dp2[i];
         
-
-
         if(isPal(s, i, s.length() - 1)) return 0;
         int cnt = INT_MAX;
         for(int k = i; k < s.length(); ++k) {
@@ -31,7 +27,7 @@ int dp1[2000][2000];
     }
 public:
     int minCut(string s) {
-        memset(dp1, -1, sizeof(dp1));
+        // memset(dp1, -1, sizeof(dp1));
         memset(dp2, -1, sizeof(dp2));
         return minCutHelp(s, 0);
     }

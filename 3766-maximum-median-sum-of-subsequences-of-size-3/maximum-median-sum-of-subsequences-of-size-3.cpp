376a3feb45c508f1a070ab7900cf44d99a117788
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-30 21:29:06 +0530

    Time: 61 ms (7.45%) | Memory: 174.5 MB (11.04%) - LeetSync

---
+++ 3766-maximum-median-sum-of-subsequences-of-size-3/maximum-median-sum-of-subsequences-of-size-3.cpp
@@ -0,0 +1,13 @@
+typedef long long ll;
+class Solution {
+public:
+    long long maximumMedianSum(vector<int>& nums) {
+        ll ans = 0;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 0; i < n / 3; i++) {
+            ans += nums[n - 2 - 2 * i];
+        }
+        return ans;
+    }
+};

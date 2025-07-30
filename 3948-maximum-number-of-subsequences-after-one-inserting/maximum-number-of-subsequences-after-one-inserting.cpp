10df6950d664bfa007f1249fc9c9fc62c6ce8ba4
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-07-30 19:28:04 +0530

    Time: 45 ms (53.13%) | Memory: 31.3 MB (78.70%) - LeetSync

---
+++ 3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    using ll = long long;
+public:
+    long long numOfSubsequences(string s) {
+        int n = s.size();
+        vector<int> pref_l(n,0), suff_t(n,0);
+        for(int i = 0; i < n; i++) {
+            if(i) pref_l[i] += pref_l[i-1];
+            if(s[i] == 'L') pref_l[i]++;
+        }
+        for(int i = n-1; i >= 0; i--) {
+            if(i+1 < n) suff_t[i] += suff_t[i+1];
+            if(s[i] == 'T') suff_t[i]++;
+        }
+
+        ll ans = 0, best = 0, ansl = 0, anst = 0;
+        for(int i = 0; i < n; i++) {
+            ll p = pref_l[i];
+            ll sf = suff_t[i];
+            if(s[i] == 'C') ans += p*sf;
+
+            if(s[i] == 'C') ansl += (p+1)*sf;
+
+            if(s[i] == 'C') anst += p*(sf+1);
+
+            best = max(best, p*sf);
+        }
+        return max({ans+best, ansl, anst});
+    }
+};

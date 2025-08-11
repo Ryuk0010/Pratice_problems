5261e2e4bc558174863c9a1cabf817385f14b097
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-08-11 21:09:50 +0530

    Time: 2 ms (46.67%) | Memory: 34.2 MB (0.00%) - LeetSync

---
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        vector<int> a1(k), a2(k);
+        int i = x;
+        int j = x + k - 1;
+        while(i <= j){
+            for(int a = y; a < k + y; a++){
+                swap(grid[i][a], grid[j][a]);
+            }
+            i++, j--;
+        }
+        return grid;
+    }
+};

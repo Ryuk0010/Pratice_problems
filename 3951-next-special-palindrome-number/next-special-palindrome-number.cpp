4b12d950b97b5320d78df648f463bdb7cf6b6362
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-08-12 20:55:53 +0530

    Time: 33 ms (55.50%) | Memory: 18.5 MB (49.42%) - LeetSync

--- 3951-next-special-palindrome-number/next-special-palindrome-number.cpp
+++ 3951-next-special-palindrome-number/next-special-palindrome-number.cpp
@@ -2,39 +2,42 @@ class Solution {
     using ll = long long;
     vector<int> mid = {0, 1, 3, 5, 7, 9};
 public:
-    ll f(int n, ll x) {
-        ll mn = 1e18;
-        for(auto &o : mid) {
-            if((o%2) != (n%2)) continue;
-            if(o > n) break;
-            string s(n, '0');
-            if(o % 2) s[n/2] = (char)('0'+o);
-            for(int bm = 0; bm < (1<<4); bm++) {
+    ll solve(int dig, ll n) {
+        ll ans = 1e18;
+        for(auto odd: mid){
+            if((odd%2) != (dig%2)) continue;
+            if(odd > dig) break;
+            string s(dig, '0');
+            if(odd%2) s[dig/2] = ('0'+odd);
+            for(int m = 0; m < (1 << 4); m++){
                 vector<int> v;
-                for(int i = 0; i < o/2; i++) v.push_back(o);
-                for(int i = 0; i < 4; i++) {
-                    if(bm & (1<<i)) {
-                        for(int x = 0; x < i + 1; x++) v.push_back(2*(i+1));
+                for(int i = 0; i < odd/2; i++) v.push_back(odd);
+                for(int i = 0; i < 4; i++){
+                    if(m & (1 << i)){
+                        for(int j = 0; j < i+1; j++) v.push_back(2*(i+1));
                     }
                 }
-                if(v.size() != n/2) continue;
+
+                if(v.size() != dig/2) continue;
                 sort(v.begin(), v.end());
-                do {
-                    for(int i = 0, j = n-1; i < n/2; i++, j--) {
-                        s[i] = s[j] = ('0' + v[i]);
+                do{
+                    for(int i = 0, j = dig-1; i < dig/2; i++, j--){
+                        s[i] = ('0' + v[i]);
+                        s[j] = ('0' + v[i]);
                     }
-                    cout << s << "\n";
                     ll val = stoll(s);
-                    if(val > x) mn = min(mn, val);
+                    if(val > n) {ans = min(val, ans); break;}
                 }
                 while(next_permutation(v.begin(), v.end()));
+
             }
+            
         }
-        return mn;
+        return ans;
     }
     
     long long specialPalindrome(long long n) {
         int dig = to_string(n).size();
-        return min(f(dig, n), f(dig+1, n));
+        return min(solve(dig, n), solve(dig+1, n));
     }
 };

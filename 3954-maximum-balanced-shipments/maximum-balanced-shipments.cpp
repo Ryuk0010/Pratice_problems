4674955244c23b948929597038e48dec9e6c89c5
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-08-14 19:37:44 +0530

    Time: 0 ms (100.00%) | Memory: 153.7 MB (41.90%) - LeetSync

---
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxBalancedShipments(vector<int>& weight) {
+        int maxi = INT_MIN;
+        int cnt = 0;
+        for(int i = 0; i < weight.size(); i++){
+            if(maxi > weight[i]){
+                cnt++;
+                maxi = 0;
+            }
+            else{
+                maxi = max(maxi, weight[i]);
+            }
+        }
+        return cnt;
+    }
+};

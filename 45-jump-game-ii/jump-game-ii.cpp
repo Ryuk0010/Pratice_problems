8b20b24c3626606407f590b96a67a553b89da232
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-08-21 13:33:53 +0530

    Time: 304 ms (5.01%) | Memory: 22.7 MB (5.51%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int pos, vector<int>& dp){
-        if(pos >= nums.size()-1) return 0;
-        if(dp[pos] != 10001) return dp[pos];
-        for(int i = 1; i <= nums[pos]; i++){
-            dp[pos] = min(dp[pos], 1 + solve(nums, pos + i, dp));
+    int solve(vector<int>& nums, int ind, vector<int>& dp){
+        if(ind >= nums.size()-1) return 0;
+        if(dp[ind] != 10001) return dp[ind];
+
+        for(int i = 1; i <= nums[ind]; i++){
+            dp[ind] = min(dp[ind], 1 + solve(nums, ind+i, dp));
         }
-        return dp[pos];
+        return dp[ind];
     }
     int jump(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 10001);
-        // return solve(nums, 0, dp);
-        dp[n-1] = 0;
-        for(int i = n-2; i >= 0; i--){
-            for(int jump = 1; jump <= nums[i]; jump++){
-                dp[i] = min(dp[i], 1+dp[min(n-1, i+jump)]);
-            }
-        }
-        return dp[0];
+        return solve(nums, 0, dp);
     }
 };

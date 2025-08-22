64761588a0ab0bb9a77ed2951bcacb6ca264d6cf
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-08-23 00:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (13.90%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    int solve(string s, int n, int ind, vector<int> dp){
-        if(ind == n) return 1;
-        if(dp[ind] != -1) return dp[ind];
-        if(s[ind] == '0') return 0;
-        int ans = solve(s, n, ind+1, dp);
-        if(ind+1 < n && (s[ind] == '1' ||(s[ind] == '2' && s[ind+1] <= '6'))){
-            ans += solve(s, n, ind+2, dp);
+    int dp[101];
+    int solve(string s, int i, int n){
+        if(i == n) return 1;
+        if(s[i] == '0') return 0;
+        if(dp[i] != -1) return dp[i];
+        int ans = 0;
+        ans += solve(s, i+1, n);
+        if(i+1 < n){
+            int num = (s[i] - '0') * 10 + (s[i+1] - '0');
+            if(num <= 26) ans += solve(s, i+2, n);
         }
-        return dp[ind] = ans;
+        return dp[i] = ans;
     }
     int numDecodings(string s) {
-        if(s[0] == '0') return 0;
+        memset(dp, -1, sizeof(dp));
         int n = s.length();
-        vector<int> dp(n+1, 0);
-        // return solve(s, n, 0, dp);
-        dp[0]=1, dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            int a = s[i-1] - '0';
-            int b = stoi(s.substr(i-2, 2));
-
-            if(a != 0) dp[i] = dp[i-1];
-            if(b >= 10 && b <= 26) dp[i] += dp[i-2];
-        }
-        return dp[n];
+        return solve(s, 0, n);
     }
 };

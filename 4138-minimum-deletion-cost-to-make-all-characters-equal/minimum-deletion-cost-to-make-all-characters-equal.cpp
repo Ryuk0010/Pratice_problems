20b3ef131c31199f8ab684d1cdbd05c463b246d2
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2025-12-21 12:26:42 +0530

    Time: 3 ms (92.31%) | Memory: 114.9 MB (7.69%) - LeetSync

--- 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.cpp
+++ 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.cpp
@@ -4,8 +4,7 @@ public:
     long long minCost(string s, vector<int>& cost) {
         vector<ll> c(26, 0);
         int n = s.length();
-        ll tc = 0;
-        ll mc = 0;
+        ll tc = 0, mc = 0;
         for(int i = 0; i < n; i++){
             c[s[i] - 'a'] += cost[i];
             tc += cost[i];

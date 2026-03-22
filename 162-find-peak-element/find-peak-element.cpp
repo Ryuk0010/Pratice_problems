57f5a1a37507986f09c81b54f6dff7bce1331ba7
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2026-03-22 20:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (38.29%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+		if(nums.size() == 1) return 0;
         int n = nums.size();
-        if(n == 1) return 0;
-        int ind = -1;
-        for(int i = 0; i < n; i++){
-            bool graterThanLeft = (i == 0) || (nums[i] > nums[i-1]);
-            bool graterThanRight = (i == n-1) || (nums[i] > nums[i+1]);
-            int element = INT_MIN;
-            if(graterThanLeft && graterThanRight){
-                if(element < nums[i]){
-                    element = nums[i];
-                    ind = i;
-                }
-            }
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        int start = 1;
+        int end = n-2;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] < nums[mid-1]) end = mid - 1;
+            else if(nums[mid] < nums[mid+1]) start = mid + 1;
         }
-        return ind;
+        return -1;
     }
 };

c4cd47e5db27110b951c4aed525e7933f48bbff1
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2026-06-14 01:48:45 +0530

    Time: 59 ms (14.36%) | Memory: 53.5 MB (9.14%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -11,9 +11,22 @@ public:
         return dp[n][amt] = min(take, nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
+        // int n = coins.size();
+        // memset(dp, -1, sizeof(dp));
+        // int ans = solve(coins, amount, n-1);
+        // return (ans >= INT_MAX - 1) ? -1 : ans;
         int n = coins.size();
-        memset(dp, -1, sizeof(dp));
-        int ans = solve(coins, amount, n-1);
-        return (ans >= INT_MAX - 1) ? -1 : ans;
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX - 1));
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int amt = 1; amt <= amount; amt++) {
+                int take = INT_MAX - 1;
+                if(amt >= coins[i - 1])
+                    take = dp[i][amt - coins[i - 1]] + 1;
+                int nottake = dp[i - 1][amt];
+                dp[i][amt] = min(take, nottake);
+            }
+        }
+        return dp[n][amount] >= INT_MAX - 1 ? -1 : dp[n][amount];
     }
 };

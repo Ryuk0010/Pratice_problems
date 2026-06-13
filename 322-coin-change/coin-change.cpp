82e2be6aab70ae91be63c471bcf78a9c9e21c54a
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2026-06-14 01:30:26 +0530

    Time: 31 ms (54.23%) | Memory: 13.6 MB (97.45%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amt, int n, vector<vector<int>>& dp){
+    int dp[12][10001];
+    int solve(vector<int>& coins, int amt, int n){
         if(n < 0) return INT_MAX-1;
         if(amt == 0) return 0;
         if(dp[n][amt] != -1) return dp[n][amt];
         int take = INT_MAX-1;
-        if(amt >= coins[n]) take = solve(coins, amt-coins[n], n, dp) + 1;
-        int nottake = solve(coins, amt, n-1, dp);
+        if(amt >= coins[n]) take = solve(coins, amt-coins[n], n) + 1;
+        int nottake = solve(coins, amt, n-1);
         return dp[n][amt] = min(take, nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = solve(coins, amount, n-1, dp);
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(coins, amount, n-1);
         return (ans >= INT_MAX - 1) ? -1 : ans;
     }
 };

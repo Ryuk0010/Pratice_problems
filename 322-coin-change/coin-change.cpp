bb41834f5a7636ac89629102211f3451a0c9d639
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2026-06-16 23:37:32 +0530

    Time: 23 ms (66.70%) | Memory: 17.8 MB (82.02%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-    int dp[12][10001];
-    int solve(vector<int>& coins, int amt, int n){
-        if(n < 0) return INT_MAX-1;
-        if(amt == 0) return 0;
-        if(dp[n][amt] != -1) return dp[n][amt];
-        int take = INT_MAX-1;
-        if(amt >= coins[n]) take = solve(coins, amt-coins[n], n) + 1;
-        int nottake = solve(coins, amt, n-1);
-        return dp[n][amt] = min(take, nottake);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        // int n = coins.size();
-        // memset(dp, -1, sizeof(dp));
-        // int ans = solve(coins, amount, n-1);
-        // return (ans >= INT_MAX - 1) ? -1 : ans;
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX - 1));
-        for(int i = 0; i <= n; i++) dp[i][0] = 0;
-        for(int i = 1; i <= n; i++) {
-            for(int amt = 1; amt <= amount; amt++) {
-                int take = INT_MAX - 1;
-                if(amt >= coins[i - 1])
-                    take = dp[i][amt - coins[i - 1]] + 1;
-                int nottake = dp[i - 1][amt];
-                dp[i][amt] = min(take, nottake);
+        vector<int> dp(amount+1, INT_MAX-1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < n; j++){
+                if(i - coins[j] >= 0)
+                dp[i] = min(dp[i], 1 + dp[i-coins[j]]); 
             }
         }
-        return dp[n][amount] >= INT_MAX - 1 ? -1 : dp[n][amount];
+        return dp[amount] == INT_MAX-1 ? -1 : dp[amount]; 
     }
 };
